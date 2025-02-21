84ccae96907c77f3014dbedf1ad422186543db60
adrianjc04 <adrianjimenezcarri@gmail.com>
2025-02-21 12:13:53 +0100

    ayudaaaa

---
+++ ayuda/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.config"},{"l":"model.config.configurables"},{"l":"model.informe"},{"l":"observer"},{"l":"view"}];updateSearchResults();
